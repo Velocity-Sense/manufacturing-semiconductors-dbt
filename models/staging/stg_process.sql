1abdd2b533d040a9c1afbe36b57faf6733d9b5e3
Gideon Fernandez <gideonfernandez@velocitysense.com>
2025-03-29 14:53:49 -0400

    Adding null check to process data

--- models/staging/stg_process.sql
+++ models/staging/stg_process.sql
@@ -7,13 +7,8 @@ with stg_process as (
 )
 
 select
-    process_id,
-    process_name,
-    process_type,
-    recipe_id,
-    expected_duration,
-    material_requirements,
-    -- extract temp and pressure from text field
-    split_part(split_part(target_parameters, 'temp:', 2), ',', 1)::float as target_temperature,
-    split_part(split_part(target_parameters, 'pressure:', 2), ',', 1)::float as target_pressure
+    *,
+    -- handle empty values with nullif before casting to float
+    nullif(split_part(split_part(target_parameters, 'temp:', 2), ',', 1), '')::float as target_temperature,
+    nullif(split_part(split_part(target_parameters, 'pressure:', 2), ',', 1), '')::float as target_pressure
 from stg_process
